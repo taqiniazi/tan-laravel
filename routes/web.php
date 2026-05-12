e88a2c5220543c7d20dec801db341f7a829fb55f
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-05-13 00:50:07 +0500

    Updated

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/admin/{any?}', function ($any = null) {
-    // If it's a request for a specific file in the admin folder
-    $path = public_path("admin/$any");
-    if ($any && file_exists($path) && !is_dir($path)) {
-        return response()->file($path);
-    }
-    
-    // Otherwise, serve the SPA index
-    return response()->file(public_path('admin/index.html'));
-})->where('any', '.*');
