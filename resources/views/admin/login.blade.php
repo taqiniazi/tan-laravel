132a52f1110a08d1955391287e4253a8e190cd5d
taqiniazi <taqiniazi61@gmail.com>
2026-06-05 02:28:53 +0500

    dsfas

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -64,7 +64,7 @@
     <div class="login-card">
         <div class="brand">
             <h2>TAN Admin</h2>
-            <p class="text-secondary">Login to manage TAN Network</p>
+            <p class="text-secondary">Login tasdfasdfo manage TAN Network</p>
         </div>
 
         @if($errors->any())
